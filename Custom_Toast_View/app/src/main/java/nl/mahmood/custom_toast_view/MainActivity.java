62a1ec0698bd074931053eb9e4f80142ca19c875
Sayed Mahmood Ghaem Maghami <pejvakco@gmail.com>
2020-04-09 23:23:10 +0200

    Create Custom_Toast_View
    create repository and first app Custom_Toast_View

---
+++ Custom_Toast_View/app/src/main/java/nl/mahmood/custom_toast_view/MainActivity.java
@@ -0,0 +1,44 @@
+package nl.mahmood.custom_toast_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunc(View view)
+    {
+        LayoutInflater inflater = getLayoutInflater();
+
+        View layout = inflater.inflate(R.layout.custom_toast,null);
+        /**
+         * In this page https://developer.android.com/guide/topics/ui/notifiers/toasts
+         * They have said to use this line instead of the top line both of them work correctly
+         * View layout = inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.custom_toast_container));
+         */
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("Hi guys! I`m custom Toast.");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
+    }
+}
